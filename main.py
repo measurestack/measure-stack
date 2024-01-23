e3a252e463fbcc520557a798d8bb53d4bcfd9446
vstampf <v.stampf@me.com>
2024-01-23 16:58:23 +0100

    towards new setup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import asyncio
-import aiohttp
 from google.cloud import bigquery
 from werkzeug.wrappers import Request
-from werkzeug.test import create_environ
-import requests
 from fastapi.templating import Jinja2Templates
 import json
 import ipaddress
@@ -11,7 +7,6 @@ from user_agents import parse
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-from tracking import track_requests
 from flask import Flask
 
 # Load environment variables from .env file (only for local testing, otherwise take env variables directly)
@@ -39,7 +34,7 @@ async def get_measure(request: Request):
 
 # consent & cookie handling
 # and data forwarding to middleware 
-# TODO this needds to be a separate function here instead of middleware (core/tracking.py)
+# TODO "middleware" needs to be a separate function here instead see below and (core/tracking.py)
 @app.route("/events", methods=["GET", "POST"])
 async def track(
     request:Request, 
@@ -66,54 +61,125 @@ async def track(
              response.delete_cookie(HASH_COOKIE_NAME)
     return response
 
-@app.get('/')
-async def main():
-    track_requests()
 
-    async with aiohttp.ClientSession() as session:
-    # Send POST request without immediately waiting for the response
-    response_task = asyncio.create_task(
-        session.post(f'{CF_URL}/track', data={'key': 'value'})
+# TODO middleware to be a proper functions block (from core/tracking.py)
+def get_hash(request):
+    hash_str = (
+        request.client.host
+        + str(request.headers.get("user-agent"))
+        + DAILY_SALT
     )
+    hash_value = hashlib.sha256(hash_str.encode()).hexdigest()
+    return hash_value
 
-@app.get("/track")
-async def track(request):
-    # Get request data as a dictionary based on the request method
-    data = {}
-    if request.method == "GET":
-        data = request.args.to_dict()
-    elif request.method == "POST":
-        data = request.form.to_dict()
-        if not data:
-            data = request.get_json()
+async def track_requests(request, call_next):
+    timestamp = datetime.datetime.now()
+    request.state.tracking_data = None
+    response: Response = await call_next(request)
+    timestamp2 = datetime.datetime.now()
+    response.background = BackgroundTask(forward_data, request, response, timestamp, timestamp2) # this background task should be triggered after the response was send to browser, hopefully request and repsonse objects are still available
+    return response
+
+async def forward_data(request, response, timestamp, timestamp2):
+    try:
+        user = get_current_user(request)
+        user_id = get_user_id(user)
+    except:
+        user_id = None
+
+    event_name = type(response).__name__.replace("Response", "") if response else "Unknown"
+    if "content-type" in response.headers:
+        event_name = response.headers.get("content-type", "").split(";")[0]
     else:
-        return "Invalid request method"
+        event_name = type(response).__name__ if response else "Unknown"
+
+    hash_value = get_hash(request)
+    is_event = True if request.state.tracking_data and request.state.tracking_data.get('en', None) else False
+
+    data = request.state.tracking_data or {}
+    data = {
+        "ts": data.get("ts", datetime.datetime.now().isoformat()),
+        "et": data.get("et", "event" if is_event else "request"),
+        "en": data.get("en", None if is_event else event_name ),
+        "p": {**data.get("p", {}), **({} if is_event else {
+            "request_duration_ms": (timestamp2-timestamp).total_seconds() * 1000,
+            "method": request.method,
+            "status_code": response.status_code,
+           })},
+        "ua": data.get("ua", None) or request.headers.get("user-agent", None),
+        "url": data.get("url", None if is_event else str(request.url)),
+        "r": data.get("r",request.headers.get("Referer", None)),
+        "c": data.get("c",request.cookies.get(CLIENT_ID_COOKIE_NAME, None)),
+        "h": data.get("h", hash_value),
+        "u": data.get("u", user_id),
+        "ch": request.client.host,
+        "ab": data.get("ab", None)
+    }
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(f"{config.TRACKING_ENDPOINT}", json=data)
+            if response.status_code != 200:
+                logger.error(f"Error sending tracking data. Status code: {response.status_code}, Response: {response.text}")
+
+            return response
+    except Exception as e:
+        logger.exception("Failed sending to tracker: ("+str(e)+") ", exc_info=e)
+
+        return
+    """
+    table_id = f"{DATASET_ID}.{TABLE_ID}"
+    client.insert_rows_json(table_id, [insert_data])
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        # Start load job
+    return
+    load_job = client.load_table_from_json(
+        [insert_data], table_id, job_config=job_config
+    )
+    print("Starting job {}".format(load_job.job_id))
+
+    # Waits for the job to complete.
+    load_job.result()  
+    """
+
+# this will return a variant for a test with name test_name. 
+# the variants dict will contain an entry for each variant and an int weight. 
+# The weights determine the propabilities of the variant. so {"default":3,"variant":1} will return variant in 25% of the cases
+# You must provide the same test_name AND variant dict to get consistent results
+# this is not random. It will determine the variant based on the user hash which is combined of IP + user_agent + daily salt
+# this should still be rather equally distirbuted except for super hashes which belong to a lot of users with the same IP
+# including the test name into the hashing will ensure that there is no correlation between variants of different tests
+def get_variant(request, test_name, variants):
+    # Check if test_name contains only a-z, 0-9, _, and -
+    if not re.match("^[a-z0-9_-]+$", test_name):
+        raise HTTPException(status_code=400, detail="Invalid test_name format. Use only a-z, 0-9, _, and -")
+
+    hash_value = request.cookies.get(HASH_COOKIE_NAME, "") or get_hash(request)
+
+    # Concatenate the hash with the test_name
+    concatenated_string = hash_value + test_name
     
-    async with aiohttp.ClientSession() as session:
-        # Send POST request without immediately waiting for the response
-        response_task = asyncio.create_task(
-            session.post('http://httpbin.org/post', data={'key': 'value'})
-        )
+    # Convert the concatenated string into a number using a hashing function
+    #hashed_value = int(hashlib.sha256(concatenated_string.encode()).hexdigest(), 16)
+    hashed_value = int.from_bytes(hashlib.sha256(concatenated_string.encode()).digest(), 'big')
+        
+    # Map the hashed value to the range [1, n]
+    n = sum(variants.values())
+    mapped_value = (hashed_value % n) + 1
+    
+    # Determine the variant based on the mapped value
+    cumulative_weight = 0
+    sortd = sorted(variants.items())
+    for variant, weight in sortd:
+        cumulative_weight += weight
+        if mapped_value <= cumulative_weight:
+            # log to tracker
+            request.state.tracking_data = {**(request.state.tracking_data or {}), "ab": (request.state.tracking_data or {}).get("ab", []) + [{"name": test_name, "variant": variant, "def": json.dumps(dict(sortd), separators=(',', ':'))}]}
 
-        # Print some text right after sending the request
-        print("Request sent, not waiting for the response...")
+            return variant
 
-        # Now wait for the response and print it
-        response = await response_task
-        data = await response.text()
-        print("Response received:")
-        print(data)
 
-    # Extract abbreviated parameter values
-    client_id = data.get("c", request.cookies.get(CLIENT_ID_COOKIE_NAME, None))
-    # TODO 
-    # call_cf('/geo_bq', data)
-    requests.post('CF_URL/process', data)
-
-@app.get("/process")
-async def route2():
-    return {"message": "This is Route 2"}
-
+# TODO old cloud function part
 def flatten(d, parent_key='', sep='_'):
     items = {}
     for k, v in d.items():
@@ -123,7 +189,6 @@ def flatten(d, parent_key='', sep='_'):
         else:
             items[new_key] = v
     return items
-
 
 def get_geoip_data(ip_address):
     db = firestore.client()
@@ -158,9 +223,8 @@ def get_geoip_data(ip_address):
         except Exception as e:
             print(f"Error retrieving data: {e}")
             return None
-        
 
-def track_old(request):
+def track(request):
     # Get request data as a dictionary based on the request method
     data = {}
     if request.method == "GET":
@@ -171,12 +235,8 @@ def track_old(request):
             data = request.get_json()
     else:
         return "Invalid request method"
-    
+
     # Extract abbreviated parameter values
-    client_id = data.get("c", request.cookies.get(CLIENT_ID_COOKIE_NAME, None))
-    # TODO 
-    # call_cf('/geo_bq', data)
-    # rest of code shouldn't be part of this function as per concept above
     timestamp = data.get("ts", datetime.datetime.now().isoformat())
     event_type = data.get("et", "event")
     event_name = data.get("en", None)
@@ -184,7 +244,7 @@ def track_old(request):
     user_agent = data.get("ua", None) or request.headers.get("user-agent", None)
     url = data.get("url", None)
     referrer = data.get("r", request.headers.get("Referer", None))
-    
+    client_id = data.get("c", request.cookies.get(CLIENT_ID_COOKIE_NAME, None))
     ab_test = data.get("ab", []) or []
     hash_value = data.get("h", None)
     user_id = data.get("u", None)
@@ -227,7 +287,6 @@ def track_old(request):
 
     location={}
     location["ip_trunc"] = client_host
-    print(type(geo_info))
     if geo_info:
         location["continent"] = geo_info.get('continent',None)
         location["country"] = geo_info.get('country',None)
@@ -252,10 +311,6 @@ def track_old(request):
         "location": location,
     }
 
-    load_to_bq(data)
-
-
-def load_to_bq(data):
     # Insert data into BigQuery
     client = bigquery.Client()
     table_ref = client.dataset(DATASET_ID).table(TABLE_ID)
@@ -266,10 +321,10 @@ def load_to_bq(data):
     else:
         return "Data inserted successfully into BigQuery"
 
-
-# Check if the script is executed directly
-if __name__ == '__main__':
-    # For local testing, call the export_firestore_to_bigquery function directly
-    # Create a WSGI environment for a GET request to '/path'
-    environ = create_environ(path='/path', method='GET',query_string={'ch': '127.0.0.1'},headers=[('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Fake-Request')])
-    track(Request(environ))
+## TODO not sure if this is needed
+# # Check if the script is executed directly
+# if __name__ == '__main__':
+#     # For local testing, call the export_firestore_to_bigquery function directly
+#     # Create a WSGI environment for a GET request to '/path'
+#     environ = create_environ(path='/path', method='GET',query_string={'ch': '102.129.204.182'},headers=[('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Fake-Request')])
+#     track(Request(environ))
